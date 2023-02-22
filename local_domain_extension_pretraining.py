f09ae15f90ac62d3c5cebc50694f3f660d5d257e
jgolde <jonas.golde@gmail.com>
2023-02-22 17:22:06 +0100

    aligned label verbalizer options for new pretraining

--- local_domain_extension_pretraining.py
+++ local_domain_extension_pretraining.py
@@ -18,25 +18,27 @@ def main(args):
         prefix=True,
     )
 
-    corpus = get_corpus(args.corpus, map="short", path=args.cache_path)
+    corpus = get_corpus(args.corpus, map=args.label_map_type)
     print(corpus)
 
     dictionary = corpus.make_label_dictionary("ner")
     print(dictionary)
 
     tars_tagger.add_and_switch_to_new_task(
-        task_name="pretraining-short", label_dictionary=dictionary, label_type="ner", force_switch=True
+        task_name="pretraining", label_dictionary=dictionary, label_type="ner", force_switch=True
     )
 
     trainer = ModelTrainer(tars_tagger, corpus)
 
     trainer.fine_tune(
-        f"{args.cache_path}/flair-models/pretrained-tart/{args.transformer}_{args.corpus}_{args.lr}-{args.seed}",
+        f"{args.cache_path}/flair-models/pretrained-tart/{args.transformer}_{args.corpus}_{args.label_map_type}_{args.lr}-{args.seed}",
         learning_rate=args.lr,
         mini_batch_size=args.bs,
         mini_batch_chunk_size=args.mbs,
         train_with_dev=True,
         monitor_test=args.monitor_test,
+        save_model_each_k_epochs=args.save_every_n_epochs,
+        max_epochs=args.epochs,
     )
 
 
@@ -46,11 +48,14 @@ if __name__ == "__main__":
     parser.add_argument("--cuda_device", type=int, default=0)
     parser.add_argument("--seed", type=int, default=123)
     parser.add_argument("--cache_path", type=str, default="/glusterfs/dfs-gfs-dist/goldejon")
-    parser.add_argument("--corpus", type=str, default="ontonotes")
+    parser.add_argument("--corpus", type=str, default="fwenerd")
     parser.add_argument("--transformer", type=str, default="bert-base-uncased")
     parser.add_argument("--lr", type=float, default=5e-5)
     parser.add_argument("--bs", type=int, default=4)
     parser.add_argument("--mbs", type=int, default=2)
     parser.add_argument("--monitor_test", type=bool, default=False)
+    parser.add_argument("--label_map_type", type=str, default="coarse-fine")
+    parser.add_argument("--save_every_n_epochs", type=int, default=0)
+    parser.add_argument("--epochs", type=int, default=10)
     args = parser.parse_args()
     main(args)
