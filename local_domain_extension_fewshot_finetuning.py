ae8753fa9d2b637e0564c0d2674e0f9842faaac3
jgolde <jonas.golde@gmail.com>
2023-02-24 16:49:42 +0100

    add zeroshot to fewshot and remove zeroshot file

--- local_domain_extension_fewshot_finetuning.py
+++ local_domain_extension_fewshot_finetuning.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from pathlib import Path
 
 import numpy as np
@@ -6,7 +7,9 @@ import numpy as np
 import flair
 from flair.datasets import ONTONOTES, ColumnCorpus
 from flair.models import FewshotClassifier, TARSTagger
+from flair.optim import LinearSchedulerWithWarmup
 from flair.trainers import ModelTrainer
+from flair.training_utils import AnnealOnPlateau
 from local_domain_extension_label_name_map import get_corpus, get_label_name_map
 
 
@@ -17,14 +20,85 @@ def main(args):
         flair.device = f"cuda:{args.cuda_device}"
 
     full_corpus = get_corpus(name=args.corpus, map=args.label_map_type)
-    average_over_support_sets = []
+
     base_save_path = Path(
         f"{args.cache_path}/flair-models/fewshot-tart/"
         f"{args.transformer}_{args.corpus}_{args.lr}_{args.seed}"
         f"_pretrained_on_{args.pretraining_corpus}{f'_{args.fewnerd_granularity}' if args.fewnerd_granularity != '' else ''}"
+        f"{'_with_early_stopping' if args.early_stopping else ''}"
         f"/{args.k}shot/"
     )
-    for split in range(args.splits):
+
+    average_over_support_sets = []
+
+    if args.k > 0:
+
+        for split in range(args.splits):
+            target_path = (
+                f"{args.cache_path}/flair-models/pretrained-tart/"
+                f"{args.transformer}_{args.pretraining_corpus}{f'_{args.fewnerd_granularity}' if args.fewnerd_granularity != '' else ''}"
+                f"_{args.lr}-{args.seed}/final-model.pt"
+            )
+            try:
+                tars_tagger: FewshotClassifier = TARSTagger.load(target_path)
+            except FileNotFoundError:
+                raise FileNotFoundError(f"{target_path} - has this model been trained?")
+
+            if args.corpus == "ontonotes":
+                support_set = ONTONOTES(label_name_map=get_label_name_map(args.corpus)).to_nway_kshot(
+                    n=-1, k=args.k, tag_type="ner", seed=split, include_validation=False
+                )
+            else:
+                support_set = ColumnCorpus(
+                    data_folder=f"data/fewshot/{args.corpus}/{args.k}shot",
+                    train_file=f"{split}.txt",
+                    sample_missing_splits=False,
+                    column_format={0: "text", 1: "ner"},
+                    label_name_map=get_label_name_map(args.corpus),
+                )
+            print(support_set)
+
+            dictionary = support_set.make_label_dictionary("ner")
+            print(dictionary)
+
+            tars_tagger.add_and_switch_to_new_task(
+                task_name="fewshot", label_dictionary=dictionary, label_type="ner", force_switch=True
+            )
+
+            trainer = ModelTrainer(tars_tagger, support_set)
+
+            trainer.fine_tune(
+                base_save_path / f"split_{split}",
+                learning_rate=args.lr,
+                mini_batch_size=args.bs,
+                mini_batch_chunk_size=args.mbs,
+                save_final_model=False,
+                max_epochs=args.epochs,
+                scheduler=AnnealOnPlateau if args.early_stopping else LinearSchedulerWithWarmup,
+                train_with_dev=args.early_stopping,
+                min_learning_rate=args.min_lr if args.early_stopping else 0.001,
+            )
+
+            result = tars_tagger.evaluate(
+                data_points=full_corpus.test,
+                gold_label_type="ner",
+                out_path=f"{base_save_path / f'split_{split}'}/predictions.txt",
+            )
+            with open(f"{base_save_path / f'split_{split}'}/result.txt", "w") as f:
+                f.write(result.detailed_results)
+
+            average_over_support_sets.append(result.main_score)
+
+        with open(
+            f"{base_save_path}/average_result.txt",
+            "w",
+        ) as f:
+            results = [round(float(x) * 100, 2) for x in average_over_support_sets]
+            f.write(f"scores: {results} \n")
+            f.write(f"average micro f1: {np.mean(results)} \n")
+            f.write(f"std micro f1: {np.std(results)} \n")
+
+    else:
         target_path = (
             f"{args.cache_path}/flair-models/pretrained-tart/"
             f"{args.transformer}_{args.pretraining_corpus}{f'_{args.fewnerd_granularity}' if args.fewnerd_granularity != '' else ''}"
@@ -35,56 +109,24 @@ def main(args):
         except FileNotFoundError:
             raise FileNotFoundError(f"{target_path} - has this model been trained?")
 
-        if args.corpus == "ontonotes":
-            support_set = ONTONOTES(label_name_map=get_label_name_map(args.corpus)).to_nway_kshot(
-                n=-1, k=args.k, tag_type="ner", seed=split, include_validation=False
-            )
-        else:
-            support_set = ColumnCorpus(
-                data_folder=f"data/fewshot/{args.corpus}/{args.k}shot",
-                train_file=f"{split}.txt",
-                sample_missing_splits=False,
-                column_format={0: "text", 1: "ner"},
-                label_name_map=get_label_name_map(args.corpus),
-            )
-        print(support_set)
-
-        dictionary = support_set.make_label_dictionary("ner")
-        print(dictionary)
-
+        dictionary = full_corpus.make_label_dictionary("ner")
         tars_tagger.add_and_switch_to_new_task(
-            task_name="fewshot", label_dictionary=dictionary, label_type="ner", force_switch=True
+            task_name="zeroshot-short", label_dictionary=dictionary, label_type="ner"
         )
 
-        trainer = ModelTrainer(tars_tagger, support_set)
-
-        trainer.fine_tune(
-            base_save_path / f"split_{split}",
-            learning_rate=args.lr,
-            mini_batch_size=args.bs,
-            mini_batch_chunk_size=args.mbs,
-            save_final_model=False,
-            max_epochs=args.epochs,
-        )
+        if not os.path.exists(base_save_path):
+            os.mkdir(base_save_path)
 
         result = tars_tagger.evaluate(
             data_points=full_corpus.test,
             gold_label_type="ner",
-            out_path=f"{base_save_path / f'split_{split}'}/predictions.txt",
+            out_path=f"{base_save_path}/predictions.txt",
         )
-        with open(f"{base_save_path / f'split_{split}'}/result.txt", "w") as f:
+        with open(
+            f"{base_save_path}/result.txt",
+            "w",
+        ) as f:
             f.write(result.detailed_results)
-
-        average_over_support_sets.append(result.main_score)
-
-    with open(
-        f"{base_save_path}/average_result.txt",
-        "w",
-    ) as f:
-        results = [round(float(x) * 100, 2) for x in average_over_support_sets]
-        f.write(f"scores: {results}")
-        f.write(f"average micro f1: {np.mean(results)}")
-        f.write(f"std micro f1: {np.std(results)}")
 
 
 if __name__ == "__main__":
